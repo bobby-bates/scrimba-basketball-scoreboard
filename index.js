dbd2f96c4004b69c8f286ed5399e32d21c51ede0
Bobby Bates <robertfbatesjr@gmail.com>
2023-11-14 15:06:45 -0500

    Add new game button & highlight leader

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 let homeCount = 0
 let guestCount = 0
-let homeCountDisplay = document.getElementById('home-counter')
-let guestCountDisplay = document.getElementById('guest-counter')
+const homeCountDisplay = document.getElementById('home-counter')
+const guestCountDisplay = document.getElementById('guest-counter')
+
+
+function highlightLeader() {
+    const removeHome = homeCountDisplay.removeAttribute('id', 'leader')
+    const removeGuest = guestCountDisplay.removeAttribute('id', 'leader')
+    
+    if (homeCount > guestCount) {
+        homeCountDisplay.setAttribute('id', 'leader')
+        removeGuest
+    } else if (guestCount > homeCount) {
+        guestCountDisplay.setAttribute('id', 'leader')
+        removeHome
+    } else {
+        removeHome
+        removeGuest
+    }
+}
 
 function incrementCount(team, points) {    
     if (team == 'home') {
@@ -11,4 +28,16 @@ function incrementCount(team, points) {
         guestCount += points
         guestCountDisplay.textContent = guestCount
     }
+    highlightLeader()
+}
+
+function newGame() {
+    homeCount = 0
+    guestCount = 0
+    homeCountDisplay.textContent = 0
+    guestCountDisplay.textContent = 0
+    // Clear any highlighted score:
+    highlightLeader()
+    
+    // Reset & start timer
 }
